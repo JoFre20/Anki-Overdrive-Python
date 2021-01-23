21852249a8292c01aaf3691752cd05926ef4fca7
JoFre20 <68242327+JoFre20@users.noreply.github.com>
2021-01-23 20:49:41 +0100

    Add files via upload

--- input.py
+++ input.py
@@ -1,8 +1,10 @@
-
+import time
 from inputs import get_gamepad
 from overdrive import Overdrive
 caradress = "EE:71:8D:6B:35:E4"
 car = Overdrive(caradress)
+numl = 0
+numr = 0
 if car.addr == caradress:
     print("car connected")
     while 1:
@@ -15,4 +17,21 @@ if car.addr == caradress:
                         carspeed = 800
 
                     car.changeSpeed(carspeed, 1000)
-                    print(carspeed)
+                else:
+                    if event.code == "BTN_TR":
+                        print("Can RIGHT")
+                        numr = numr+1
+                        print(numr)
+                        if numr == 2:
+                            car.changeLaneRight(carspeed, 800)
+                            print("RIGHT")
+                            numr = 0
+                    else:
+                        if event.code == "BTN_TL":
+                            print("Can LEFT")
+                            numl= numl+1
+                            print(numl)
+                            if numl == 2:
+                                car.changeLaneLeft(carspeed, 800)
+                                print("LEFT")
+                                numl = 0
