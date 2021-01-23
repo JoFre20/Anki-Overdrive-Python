6ee9b2b0858b95da297dbb4bf9628f3df1922924
JoFre20 <68242327+JoFre20@users.noreply.github.com>
2021-01-23 18:30:21 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from overdrive import Overdrive
-
-caradress = "EE:71:8D:6B:35:E4"
-car = Overdrive(caradress)
-if car.addr == caradress:
-    print("Online")
-    car.sendCommand("set-engine-lights 255 0 0 STEADY 0")
