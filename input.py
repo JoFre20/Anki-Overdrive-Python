c45ef2a07058fc4133445153ce69d73b5a96f6c3
JoFre20 <68242327+JoFre20@users.noreply.github.com>
2021-01-24 09:29:17 +0100

    Add files via upload

--- input.py
+++ input.py
@@ -3,8 +3,11 @@ from inputs import get_gamepad
 from overdrive import Overdrive
 caradress = "EE:71:8D:6B:35:E4"
 car = Overdrive(caradress)
+numa = 0
 numl = 0
 numr = 0
+mcs = 800
+nums = 0
 if car.addr == caradress:
     print("car connected")
     while 1:
@@ -12,26 +15,43 @@ if car.addr == caradress:
         for event in events:
             if not event.ev_type == "Sync":
                 if event.code == "ABS_RZ":
-                    carspeed = event.state
-                    if carspeed > 800:
-                        carspeed = 800
-
-                    car.changeSpeed(carspeed, 1000)
+                        carspeed = event.state
+                        if carspeed > mcs:
+                            carspeed = mcs
+                        car.changeSpeed(carspeed, 1000)
+                        print(carspeed)
                 else:
                     if event.code == "BTN_TR":
-                        print("Can RIGHT")
                         numr = numr+1
-                        print(numr)
                         if numr == 2:
-                            car.changeLaneRight(carspeed, 800)
+                            car.changeLaneRight(500, 800)
                             print("RIGHT")
                             numr = 0
                     else:
                         if event.code == "BTN_TL":
-                            print("Can LEFT")
                             numl= numl+1
-                            print(numl)
                             if numl == 2:
-                                car.changeLaneLeft(carspeed, 800)
+                                car.changeLaneLeft(500, 800)
                                 print("LEFT")
                                 numl = 0
+                        else:
+                            if event.code == "BTN_SOUTH":
+                                numa = numa+1
+                                print(numa)
+                                if numa == 2:
+                                    mcs = 1000
+                                    car.changeSpeed(carspeed,1000)
+                                    time.sleep(1)
+                                    mcs = 800
+                                    car.changeSpeed(carspeed, 1000)
+                                    numa = 0
+                            else:
+                                if event.code == "BTN_SELECT":
+                                    nums = nums+1
+                                    if nums == 2:
+                                       car.changeSpeed(600,1000)
+                                    else:
+                                        if nums == 4:
+                                            car.changeSpeed(0,1000)
+                                            nums = 0
+
